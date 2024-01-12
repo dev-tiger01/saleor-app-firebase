2c5644768c0ea3a7d160ecb20ebb9cd7691a2828
Jan Mwesigwa <44441026+madmonkeyworks@users.noreply.github.com>
2024-01-12 17:13:35 +0100

    Replace saleor.app

--- src/saleor-app.ts
+++ src/saleor-app.ts
@@ -1,20 +1,11 @@
 import { APL, FileAPL, SaleorCloudAPL, UpstashAPL } from "@saleor/app-sdk/APL";
 import { SaleorApp } from "@saleor/app-sdk/saleor-app";
-import { AwsGraphQLAPL } from "@ecomm/APL";
-import awsconfig from '../aws-exports';
 
 const aplType = process.env.APL ?? "file";
 
 export let apl: APL;
 
 switch (aplType) {
-  case "aws-graphql": 
-    if (!process.env.APP_API_BASE_URL) {
-      throw new Error("APP_API_BASE_URL is not configured - missing env variables. Check saleor-app.ts");
-    }
-    apl = new AwsGraphQLAPL(awsconfig, process.env.APP_API_BASE_URL);
-
-    break;
   case "upstash":
     apl = new UpstashAPL();
 
@@ -42,5 +33,3 @@ switch (aplType) {
 export const saleorApp = new SaleorApp({
   apl,
 });
-
-export const REQUIRED_SALEOR_VERSION = ">=3.11.7 <4";
